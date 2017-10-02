d9b17a123b602f01f5f0f873b3ec08e63ff0a018
Timo Kaufmann <eisfreak7@gmail.com>
2017-10-02 16:12:16 -0500

    Add schooner preprocessing job

---
+++ job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --partition=normal
+#SBATCH --ntasks=5
+#SBATCH --mem=2048
+#SBATCH --output=preprocess_%J_stdout.log
+#SBATCH --error=preprocess_%J_stderr.log
+#SBATCH --time=1:00:00
+#SBATCH --job-name=github-processing
+#SBATCH --mail-user=timo.kaufmann@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --workdir=/home/timo
+#
+#################################################
+python /home/timo/preprocess.py
