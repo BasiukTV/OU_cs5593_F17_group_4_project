cb8acf65b7579b5224da115c2f9adb39a4fbc48c
Taras Basiuk <BasiukTV@gmail.com>
2017-10-09 14:06:16 -0500

    Making preprocess.py importable and moving it to Python3 syntax.

--- src/preproc/preprocess.py
+++ src/preproc/preprocess.py
@@ -1,12 +1,10 @@
-import multiprocessing
-
-print("Job started")
+import json, multiprocessing
 
 def pretty_print(obj):
     print(json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': ')))
 
-def process_day((month, day)):
-    import json
+def process_day(month_and_day):
+    (month, day) = month_and_day
     starcount = 0
     for hour in range (0,24):
         filename = '/scratch/timo/github/2016-{:02}-{:02}-{}.json'.format(month, day, hour)
@@ -17,14 +15,18 @@ def process_day((month, day)):
                     t = obj["type"]
                     if t == "WatchEvent":
                         starcount += 1
-        except IOError:
+        except IOError as er:
+            print(er)
             pass
+
     print("{} new stars on 2017-{:02}-{:02}".format(starcount, month, day))
 
 
 def process_month(month):
     out = pool.map(process_day, zip([month] * 31, range(1,32)))
 
-pool = multiprocessing.Pool(50)
-for month in range(1,4):
-    process_month(month)
+if __name__ == '__main__':
+    print("Job started")
+    pool = multiprocessing.Pool(50)
+    for month in range(1,4):
+        process_month(month)
