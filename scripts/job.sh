e94427ff0d8af65bbdc9577a6a9c09219278e318
Timo Kaufmann <eisfreak7@gmail.com>
2017-10-27 22:41:11 -0500

    Increase running time and RAM

--- scripts/job.sh
+++ scripts/job.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --partition=normal
 #SBATCH --ntasks=1
-#SBATCH --mem=4096
+#SBATCH --mem=8192
 #SBATCH --output=preprocess_%J_stdout.log
 #SBATCH --error=preprocess_%J_stderr.log
-#SBATCH --time=12:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name=github-processing
 #SBATCH --mail-user=timo.kaufmann@ou.edu
 #SBATCH --mail-type=ALL
