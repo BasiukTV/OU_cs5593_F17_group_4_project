adc01234b8ada7e895a3d5cd50ea1d8594803d59
Timo Kaufmann <eisfreak7@gmail.com>
2017-10-29 13:25:11 -0500

    Load python3 module before using python3

--- scripts/job.sh
+++ scripts/job.sh
@@ -16,6 +16,7 @@ scratch="/scratch/timo"
 indir="${scratch}/github"
 outdir="${scratch}/out-${SLURM_JOB_ID}"
 mkdir -p "${outdir}"
+module load Python/3.5.1-intel-2016a
 python3 /home/timo/repo/src/preproc/parse_json.py \
 	--indir "${indir}" \
 	--outdir "${outdir}" \
