f5c8ae03528174b18e6831df948aac7e4b925e05
Timo Kaufmann <timokau@zoho.com>
2017-11-12 15:56:06 -0600

    Adjust slurm script

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -5,16 +5,16 @@
 #SBATCH --mem=8192
 #SBATCH --output=preprocess_%J_stdout.log
 #SBATCH --error=preprocess_%J_stderr.log
-#SBATCH --time=48:00:00
+#SBATCH --time=60:00:00
 #SBATCH --job-name=github-processing
 #SBATCH --mail-user=timo.kaufmann@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --workdir=/scratch/timo
+#SBATCH --workdir=/work/timo
 #
 #################################################
-scratch="/scratch/timo"
+scratch="/work/timo"
 indir="${scratch}/github"
-outdir="${scratch}/out-${SLURM_JOB_ID}"
+outdir="${scratch}/parsed-${SLURM_JOB_ID}"
 mkdir -p "${outdir}"
 module load Python/3.5.1-intel-2016a
 python3 /home/timo/repo/src/preproc/parse_json.py \
