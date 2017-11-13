cfc748b5327b0de769fdea5c3f0173411f5979bd
Timo Kaufmann <timokau@zoho.com>
2017-11-13 17:09:50 -0600

    Flush logging

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,11 +1,13 @@
 from time import gmtime, strftime
-from sys import stderr
+from sys import stderr, stdout
 
 def now():
     return strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 def log(string, *args):
     print("{}: {}".format(now(), string).format(*args))
+    stdout.flush()
 
 def elog(string, *args):
     print("{}: {}".format(now(), string).format(*args), file=stderr)
+    stderr.flush()
