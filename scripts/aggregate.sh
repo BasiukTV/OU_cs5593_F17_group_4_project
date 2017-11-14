04ffb12bfae03a90203c6dc0de61427d944b5c82
Timo Kaufmann <timokau@zoho.com>
2017-11-14 14:37:22 -0600

    Fix slurm args

--- scripts/aggregate.sh
+++ scripts/aggregate.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=normal
 #SBATCH --ntasks=1
 #SBATCH --mem=8192
-#SBATCH --output=preprocess_%J_stdout.log
-#SBATCH --error=preprocess_%J_stderr.log
-#SBATCH --time=60:00:00
+#SBATCH --output=out-%J.log
+#SBATCH --error=err-%J.log
+#SBATCH --time=48:00:00
 #SBATCH --job-name=github-processing
 #SBATCH --mail-user=timo.kaufmann@ou.edu
 #SBATCH --mail-type=ALL
@@ -14,8 +14,8 @@
 #################################################
 scratch="/work/timo"
 indir="${scratch}/parsed"
-outdir="${scratch}/aggregated-${SLURM_JOB_ID}"
+# outdir="${scratch}/aggregated-${SLURM_JOB_ID}"
 mkdir -p "${outdir}"
 module load Python/3.5.1-intel-2016a
 python3 /home/timo/repo/src/preproc/aggregate.py \
-	--file "${outdir}/preprocessed.sqlite3"
+	--file "${indir}/preprocessed.sqlite3"
