ff5050258274240b08d7421b127697c169226a39
Timo Kaufmann <timokau@zoho.com>
2017-11-14 14:30:43 -0600

    Add aggregate slurm script

---
+++ scripts/aggregate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#SBATCH --partition=normal
+#SBATCH --ntasks=1
+#SBATCH --mem=8192
+#SBATCH --output=preprocess_%J_stdout.log
+#SBATCH --error=preprocess_%J_stderr.log
+#SBATCH --time=60:00:00
+#SBATCH --job-name=github-processing
+#SBATCH --mail-user=timo.kaufmann@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --workdir=/work/timo
+#
+#################################################
+scratch="/work/timo"
+indir="${scratch}/parsed"
+outdir="${scratch}/aggregated-${SLURM_JOB_ID}"
+mkdir -p "${outdir}"
+module load Python/3.5.1-intel-2016a
+python3 /home/timo/repo/src/preproc/aggregate.py \
+	--file "${outdir}/preprocessed.sqlite3"
