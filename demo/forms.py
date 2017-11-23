70c5d47b0565539476f1141573de4b71daba9a0b
Redae <redae.beraki@gmail.com>
2017-11-23 14:34:58 -0600

    added Form templates for preprocessing, clustering and regression

--- demo/forms.py
+++ demo/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import Form 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
-class DatasetForm(Form):
-    fpath = StringField('File Path', validators=[DataRequired("Please enter the dataset path.")])
-    submit = SubmitField('Train Model')
+class PreprocessingForm(Form):
+    fpath = StringField('Dataset Path:', validators=[DataRequired("Please enter the dataset path.")])
+    submit = SubmitField('Proceed')
 
+class ClusteringForm(Form):
+    clusteringAlgorithms = RadioField('Clustering Algorithms:', choices=[(1, 'Simple K-means'), (2, 'Hierarchical')])
+    submit = SubmitField('Proceed')
+
+class RegressionForm(Form):
+    regressionAlgorithms = RadioField('Regression Algorithms:', choices=[(1, 'Logistic Regression'), (2, 'Linear Regression')])
+    submit = SubmitField('Proceed')
