2a31ce96df928d24a75372c162e0d2bfa0cf3c68
Redae <redae.beraki@gmail.com>
2017-11-25 23:55:47 -0600

    changed StringField to FileField in forms

--- demo/forms.py
+++ demo/forms.py
@@ -1,15 +1,16 @@
-from flask_wtf import Form 
+from flask_wtf import Form
+from flask_wtf.file import FileField
 from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
 class PreprocessingForm(Form):
-    fpath = StringField('Dataset Path:', validators=[DataRequired("Please enter the dataset path.")])
+    fpath = FileField('Your JSON File: ', validators=[DataRequired("Please enter the dataset path.")])
     submit = SubmitField('Proceed')
 
 class ClusteringForm(Form):
-    clusteringAlgorithms = RadioField('Clustering Algorithms:', choices=[(1, 'Simple K-means'), (2, 'Hierarchical')])
+    clusteringAlgorithms = RadioField('Clustering Algorithms:', choices=[(1, 'Simple K-means'), (2, 'Hierarchical')], default = 1)
     submit = SubmitField('Proceed')
 
 class RegressionForm(Form):
-    regressionAlgorithms = RadioField('Regression Algorithms:', choices=[(1, 'Logistic Regression'), (2, 'Linear Regression')])
+    regressionAlgorithms = RadioField('Regression Algorithms:', choices=[(1, 'Logistic Regression'), (2, 'Linear Regression')], default = 1)
     submit = SubmitField('Proceed')
