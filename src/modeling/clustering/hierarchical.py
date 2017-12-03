f98342f56d450f0b076f645ee6ec95701d50aea9
Taras Basiuk <BasiukTV@gmail.com>
2017-12-02 18:52:43 -0600

    Actualy run hierarchical clustering. Don't output any results yet. This is for #22

--- src/modeling/clustering/hierarchical.py
+++ src/modeling/clustering/hierarchical.py
@@ -8,6 +8,7 @@ sys.path.insert(0, app_src_dir)
 
 from modeling.modeling import Modeling
 from modeling.clustering.cluster_model import ClusterModel
+from preproc.record.contributor import calc_avg_contribution
 
 from utils.logging import log, progress
 
@@ -73,18 +74,85 @@ class HierarchicalModeling(Modeling):
         progress(trial_size, trial_size, last=True)
 
         log("Trial #{} : Building initial proximity matrix.".format(trial_num))
-        proximity = {}
+        proximity_matrix = {}
         for i in range(trial_size):
             c1_ID = trial_contributorIDs[i]
-            proximity[c1_ID] = {}
+            proximity_matrix[c1_ID] = ({c1_ID}, {}) # ({members of cluster}, {distances to other clusters})
 
-            # We will only build upper triangular part of the matrix as it is symetric 0-diagonal. 
+            # We will only build upper triangular part of the matrix as it is symetric and 0-diagonal.
             for j in range(i + 1, trial_size):
                 c2_ID = trial_contributorIDs[j]
 
-                proximity[c1_ID][c2_ID] = trial_avg_contributions[c1_ID].eucld_dist(
+                proximity_matrix[c1_ID][1][c2_ID] = trial_avg_contributions[c1_ID].eucld_dist(
                     trial_avg_contributions[c2_ID],
                     weights=trial_weights)
+
+        log("Trial #{} : Starting to merge clusters in.".format(trial_num))
+        while len(proximity_matrix) > 1:
+            # Arbitrarily pick one distance between clusters as a min distance
+
+            """
+                Below relies on iterator picking top row of the proximity matrix.
+                If it picks bottom row second iterator will fail to retieve next item.
+
+                TODO Fix this.
+            """
+            min_dist_from_clusterID = next(iter(proximity_matrix.keys()))
+            min_dist_to_clusterID = next(iter(proximity_matrix[min_dist_from_clusterID][1].keys()))
+
+            # Now find actual minimal distance betweem clusters
+            for i in proximity_matrix.keys():
+                for j in proximity_matrix[i][1].keys():
+                    if proximity_matrix[i][1][j] < proximity_matrix[min_dist_from_clusterID][1][min_dist_to_clusterID]:
+                        min_dist_from_clusterID = i
+                        min_dist_to_clusterID = j
+
+            # Move all members of min_dist_to_clusterID into min_dist_from_clusterID
+            proximity_matrix[min_dist_from_clusterID] = (proximity_matrix[min_dist_from_clusterID][0].union(
+                proximity_matrix[min_dist_to_clusterID][0]), proximity_matrix[min_dist_from_clusterID][1])
+
+            # Remove row of proximity_matrix corresponding to min_dist_to_clusterID
+            del proximity_matrix[min_dist_to_clusterID]
+            # Remove min_dist_to_clusterID entry from proximity_matrix[min_dist_from_clusterID][1]
+            del proximity_matrix[min_dist_from_clusterID][1][min_dist_to_clusterID]
+
+            # Prepare new cluster average contribution
+            new_cluster_contributor_records = []
+            for cID in proximity_matrix[min_dist_from_clusterID][0]:
+                new_cluster_contributor_records.append(trial_avg_contributions[cID])
+            new_cluster_average_contribution = calc_avg_contribution(new_cluster_contributor_records)
+
+            # Iterate through remaining rows of proximity matrix and update them
+            for cID1 in proximity_matrix.keys():
+                if cID1 == min_dist_from_clusterID: # Distances update for this row will be done bu other iterations
+                    continue
+
+                # If this cluster contains distance to min_dist_to_clusterID remove it
+                if min_dist_to_clusterID in proximity_matrix[cID1][1].keys():
+                    del proximity_matrix[cID1][1][min_dist_to_clusterID]
+
+                # Prepare this cluster average contribution
+                # TODO This cluster membership didn't change, we don't need to recalculate its average contribution
+                cluster_contributor_records = []
+                for cID2 in proximity_matrix[cID1][0]:
+                    cluster_contributor_records.append(trial_avg_contributions[cID2])
+                cluster_average_contribution = calc_avg_contribution(cluster_contributor_records)
+
+                # If this cluster contains distance to new cluster update it
+                if cID1 in proximity_matrix[min_dist_from_clusterID][1].keys():
+                    proximity_matrix[min_dist_from_clusterID][1][cID1] = new_cluster_average_contribution.eucld_dist(
+                        other=cluster_average_contribution,
+                        weights=trial_weights,
+                        self_cluster_size=len(proximity_matrix[min_dist_from_clusterID][0]),
+                        other_cluster_size=len(proximity_matrix[cID1][0]))
+                    continue
+
+                # Otherwise new cluster contains distance to this distance, update it
+                proximity_matrix[cID1][1][min_dist_from_clusterID] = new_cluster_average_contribution.eucld_dist(
+                        other=cluster_average_contribution,
+                        weights=trial_weights,
+                        self_cluster_size=len(proximity_matrix[min_dist_from_clusterID][0]),
+                        other_cluster_size=len(proximity_matrix[cID1][0]))
 
         log("Trial #{} : Done.".format(trial_num))
         log("Hierarchical clustering is done.")
